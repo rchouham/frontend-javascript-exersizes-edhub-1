b6dbed7422737af6f7cf810b5cf830449cb6a6c3
Your Name <you@example.com>
2021-01-12 22:29:38 +0100

    mathematical exercise

--- b-mathematical-operators.js
+++ b-mathematical-operators.js
@@ -6,37 +6,48 @@
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9
 // ==========================================
 
-
+let sum = 4+5
+console.log(sum)
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364
 // ==========================================
 
+let multiply = 7*52
+console.log(multiply)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49
 // ==========================================
-
+let total = 4+3
+console.log(total * 7)
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12
 // ==========================================
-
+let devine = 36/6
+console.log(devine * 2)
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig'
 // ==========================================
-
+let smaak1 = "zoet"
+let smaak2 ="sappig"
+console.log(smaak1+smaak2)
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw volledige naam en plak bovenstaande variabelen aan elkaar.
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters'
 // ==========================================
-
-
+let voornaam = "Rachid"
+let achternaam= "Chouham"
+console.log(voornaam + " " + achternaam)
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg
 // ==========================================
+let city= "Nieuwegein"
+let provincie="Utrecht"
+console.log(city+ "," + " "+provincie)
