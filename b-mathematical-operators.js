a5d9c122762bcde200e576a242f81f16289fd84c
Your Name <you@example.com>
2021-01-13 11:35:33 +0100

    arrays exercise

--- b-mathematical-operators.js
+++ b-mathematical-operators.js
@@ -7,33 +7,33 @@
 // ==========================================
 
 let sum = 4+5
-console.log(sum)
+    console.log(sum)
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364
 // ==========================================
 
 let multiply = 7*52
-console.log(multiply)
+    console.log(multiply)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49
 // ==========================================
 let total = 4+3
-console.log(total * 7)
+    console.log(total * 7)
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12
 // ==========================================
 let devine = 36/6
-console.log(devine * 2)
+    console.log(devine * 2)
 
 // ==========================================
 // 5. Declareer twee variabelen en zet daarin respectievelijk de waardes 'zoet' en 'sappig'.
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig'
 // ==========================================
-let smaak1 = "zoet"
-let smaak2 ="sappig"
-console.log(smaak1+smaak2)
+let smaak = "zoet"
+    smaak1 ="sappig"
+    console.log(smaak+smaak1)
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
@@ -41,13 +41,13 @@ console.log(smaak1+smaak2)
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters'
 // ==========================================
 let voornaam = "Rachid"
-let achternaam= "Chouham"
-console.log(voornaam + " " + achternaam)
+    achternaam= "Chouham"
+    console.log(voornaam + " " + achternaam)
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg
 // ==========================================
 let city= "Nieuwegein"
-let provincie="Utrecht"
-console.log(city+ "," + " "+provincie)
+    provincie="Utrecht"
+    console.log(city+ "," + " "+provincie)
--- c-arrays.js
+++ c-arrays.js
@@ -2,35 +2,52 @@
 // 1. Declareer een variabele en sla daar een array met vier jaartallen in op.              // bijv. 2020, 2019, 2018 en 2017
 // ==========================================
 
+let  year = [2020,2019,2018,2017];
+     year.length
+     console.log(year.length)
+     console.log(year)
+     console.log(year[2])
+
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies in op.         // bijv. schaken, koekjes eten en muziek luisteren
 // ==========================================
-
+let hobbies = ["sports", "reading","motor"]
+    onsole.log(hobbies)
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Log de waarde 3 uit de array in de terminal                                          // geeft 3
 // ==========================================
-
+let nummerreeks =[3,4,5,6,7,2]
+    console.log(nummerreeks[0])
 
 // ==========================================
 // 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje
 // 4b. Log de waarde blauw uit de array in de terminal                                      // geeft blauw
 // 4c. Log de waarde geel uit de array in de terminal                                       // geeft geel
 // ==========================================
+let colors =["groen","geel","rood","paars","blauw","oranje",]
 
+   console.log(colors)
+   console.log(colors[1])
+   console.log(colors[4])
 
 // ==========================================
 // 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25
 // 5b. Pas een van de items in de array aan zodat de cijferreeks netjes doorloopt
 // 5c. Log de de array in de terminal                                                       // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
-
-
+let reeks =[21,22,23,25,25]
+    reeks[3]=24
+    console.log(reeks)
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
 // 6b. Verander daarna de waarde bladerdeeg in lasagne bladen
 // 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+let ingredienten =["bladerdeeg","knoflook","spinazie"]
+    ingredienten[0]="lasagne bladen"
+    ingredienten.length
+    console.log(ingredienten)
